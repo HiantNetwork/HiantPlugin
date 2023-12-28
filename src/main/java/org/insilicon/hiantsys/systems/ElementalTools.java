057d7fd0e0ae354d554ecd56ee6209d4c1cd7513
Jack <jackbaker321@icloud.com>
2023-12-27 22:01:04 -0600

    Fixing merge issues
    Some issues still remained with naming with HiantSys had to fix with a quick global patch

--- src/main/java/org/insilicon/hiantsys/systems/ElementalTools.java
+++ src/main/java/org/insilicon/hiantsys/systems/ElementalTools.java
@@ -3,14 +3,15 @@ package org.insilicon.hiantsys.systems;
 import org.bukkit.Location;
 import org.bukkit.NamespacedKey;
 import org.bukkit.Particle;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
 import org.insilicon.hiantsys.Hiantsys;
-
 public class ElementalTools implements Listener {
 
     public NamespacedKey key = Hiantsys.getPlugin(Hiantsys.class).key;
@@ -20,6 +21,55 @@ public class ElementalTools implements Listener {
     }
 
 
+    @EventHandler
+    public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
+
+        if (event.getDamager() instanceof Player && event.getEntity() instanceof Player) {
+            System.out.println("Both players");
+        } else {
+            return;
+        }
+
+        Player attacker = (Player) event.getDamager();
+        Player victim = (Player) event.getEntity();
+
+
+        ItemStack item = attacker.getInventory().getItemInMainHand();
+        System.out.println(item);
+
+        System.out.println(Hiantsys.getPlugin(Hiantsys.class).key);
+
+        if (item.getItemMeta().getPersistentDataContainer().has(Hiantsys.getPlugin(Hiantsys.class).key, org.bukkit.persistence.PersistentDataType.STRING)) {
+            System.out.println("Player killed with ElementalItem");
+
+            //Get the value of the ElementalItem
+            String ElementalValue = item.getItemMeta().getPersistentDataContainer().get(Hiantsys.getPlugin(Hiantsys.class).key, org.bukkit.persistence.PersistentDataType.STRING);
+
+
+            if (ElementalValue.equals("Fire")) {
+
+                //Make multiple fire particles fly out of the victim like an explosion
+                spawnFireExplosion(victim.getLocation(), 0.5, 300);
+
+                if (attacker != null) {
+                    // Play sound for the killer
+                    //Check if the item they are holding is a bow
+                    if (attacker.getInventory().getItemInMainHand().getType().toString().toLowerCase().contains("bow")) {
+                        Sound soundForKiller = Sound.BLOCK_NOTE_BLOCK_PLING; // Example sound
+                        attacker.playSound(attacker.getLocation(), soundForKiller, 0.5f, 1.0f);
+                    } else {
+
+                    }
+
+
+
+                }
+
+
+            }
+        }
+
+    }
 
 
     //On Kill Event
@@ -55,8 +105,17 @@ public class ElementalTools implements Listener {
             if (ElementalValue.equals("Fire")) {
 
                 //Make multiple fire particles fly out of the victim like an explosion
-                spawnFireExplosion(victim.getLocation());
+                spawnFireExplosion(victim.getLocation(), 1, 700);
 
+                if (attacker != null) {
+                    // Play sound for the killer
+                    Sound soundForKiller = Sound.ITEM_FIRECHARGE_USE; // Example sound
+                    attacker.playSound(attacker.getLocation(), soundForKiller, 1.0f, 1.0f);
+
+                    // Play sound for the victim
+                    Sound soundForVictim = Sound.ITEM_FIRECHARGE_USE; // Example sound
+                    victim.playSound(victim.getLocation(), soundForVictim, 1.0f, 1.0f);
+                }
 
 
             }
@@ -69,14 +128,16 @@ public class ElementalTools implements Listener {
 
     //Animations
 
-    private void spawnFireExplosion(Location location) {
-        int numberOfParticles = 700; // Adjust the number of particles as needed
-        double speed = 1; // Increased speed for a more explosive effect
+    private void spawnFireExplosion(Location location, double speed, int numberOfParticles) {
+
 
         for (int i = 0; i < numberOfParticles; i++) {
             Vector direction = Vector.getRandom().subtract(new Vector(0.5, 0.5, 0.5)).normalize();
             location.getWorld().spawnParticle(Particle.FLAME, location, 0, direction.getX(), direction.getY(), direction.getZ(), speed);
         }
+
+        //Play fire_charge sound
+
     }
 
 
