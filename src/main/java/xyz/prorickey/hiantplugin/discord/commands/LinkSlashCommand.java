a2f0a63449fbeb04cb7fd8382b3c1c4fe3ce5e3d
Trevor Bedson <78997566+Prorickey@users.noreply.github.com>
2024-07-22 18:14:16 -0400

    linking works

--- src/main/java/xyz/prorickey/hiantplugin/discord/commands/LinkSlashCommand.java
+++ src/main/java/xyz/prorickey/hiantplugin/discord/commands/LinkSlashCommand.java
@@ -3,9 +3,12 @@ package xyz.prorickey.hiantplugin.discord.commands;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.insilicon.hiantplugin.HiantPlugin;
 import org.jetbrains.annotations.NotNull;
 import xyz.prorickey.hiantplugin.commands.LinkCommand;
+import xyz.prorickey.hiantplugin.database.Database;
 
 public class LinkSlashCommand extends ListenerAdapter {
 
@@ -13,37 +16,39 @@ public class LinkSlashCommand extends ListenerAdapter {
     public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
         if (!event.getName().equals("link")) return; // Not our slash command
         OptionMapping codeMapping = event.getOption("code");
-        if(HiantPlugin.getDatabase().getLinkedAccount(event.getIdLong()) != null) {
-            event.reply("You already have a linked account.")
+        Database.LinkedAccount linkedAccount = HiantPlugin.getDatabase().getLinkedAccount(event.getUser().getIdLong());
+        if(linkedAccount != null) {
+            OfflinePlayer player = Bukkit.getOfflinePlayer(linkedAccount.getMinecraftUUID());
+            event.reply("Your account is already linked to `" + player.getName() + "`. If you want to unlink, use `/unlink`.")
                     .setEphemeral(true)
                     .queue();
-            return;
-        }
-        if(codeMapping != null) {
-            String code = codeMapping.getAsString();
-            if (LinkCommand.codes.containsKey(code)) {
-                LinkCommand.LinkCode linkCode = LinkCommand.codes.get(code);
-                LinkCommand.minecraftLinking.remove(linkCode.getUuid());
-                LinkCommand.codes.remove(code);
-                HiantPlugin.getDatabase().linkAccount(event.getUser().getIdLong(), linkCode.getUuid().toString());
-                LinkCommand.recentlyLinked.put(linkCode.getUuid(), event.getUser().getName());
-                event.reply("Your account has been linked successfully!")
-                        .setEphemeral(true)
-                        .queue();
+        } else {
+            if(codeMapping != null) {
+                String code = codeMapping.getAsString();
+                if (LinkCommand.codes.containsKey(code)) {
+                    LinkCommand.LinkCode linkCode = LinkCommand.codes.get(code);
+                    LinkCommand.minecraftLinking.remove(linkCode.getUuid());
+                    LinkCommand.codes.remove(code);
+                    HiantPlugin.getDatabase().linkAccount(event.getUser().getIdLong(), linkCode.getUuid().toString());
+                    LinkCommand.recentlyLinked.put(linkCode.getUuid(), event.getUser().getName());
+                    event.reply("Your account has been linked successfully!")
+                            .setEphemeral(true)
+                            .queue();
+                } else {
+                    event.reply("Invalid code.")
+                            .setEphemeral(true)
+                            .queue();
+                }
             } else {
-                event.reply("Invalid code.")
+                String code = LinkCommand.generateCode();
+                LinkCommand.codes.put(code, new LinkCommand.LinkCode(code, null, event.getUser().getIdLong()));
+                LinkCommand.discordLinking.put(event.getUser().getIdLong(), System.currentTimeMillis());
+                event.reply("Your link code is `" + code + "`. Do not give this to anyone." +
+                                "\n\n" +
+                                "In the Minecraft server, execute `/link " + code + "` to link your account.")
                         .setEphemeral(true)
                         .queue();
             }
-        } else {
-            String code = LinkCommand.generateCode();
-            LinkCommand.codes.put(code, new LinkCommand.LinkCode(code, null, event.getUser().getIdLong()));
-            LinkCommand.discordLinking.put(event.getUser().getIdLong(), System.currentTimeMillis());
-            event.reply("Your link code is `" + code + "`. Do not give this to anyone." +
-                    "\n\n" +
-                    "In the Minecraft server, execute `/link " + code + "` to link your account.")
-                    .setEphemeral(true)
-                    .queue();
         }
     }
 }
