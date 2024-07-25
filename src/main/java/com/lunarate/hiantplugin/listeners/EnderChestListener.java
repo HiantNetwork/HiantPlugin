f30826106a6bb4cfa18d4bfc69f1262c1940c4bd
Trevor Bedson <78997566+Prorickey@users.noreply.github.com>
2024-07-25 15:21:35 -0400

    No imports?

--- src/main/java/com/lunarate/hiantplugin/listeners/EnderChestListener.java
+++ src/main/java/com/lunarate/hiantplugin/listeners/EnderChestListener.java
@@ -1,6 +1,14 @@
 package com.lunarate.hiantplugin.listeners;
 
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+
 public class EnderChestListener implements Listener {
     @EventHandler
     public void onInventoryOpen(InventoryOpenEvent event) {
